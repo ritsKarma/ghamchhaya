77c7599b6f28a58456e23c78fbdfd69fd2ada79b
Rits Karma <ritsi@live.com>
2021-12-12 18:02:22 +0100

    after working toggle menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+btnHamburger.addEventListener('click', function () {
+    console.log('click hamburger');
 
+    if (header.classList.contains('open')) {
+        //Close the Hamburger menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else {
+        //open hamburger munu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+});
